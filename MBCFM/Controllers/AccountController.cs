50fb09e134ac4bf4325ce4061d5662101b447c5e
unknown <jprice@petdsk002543.corp.cpicardgroup.com>
2014-12-11 17:07:34 +0000

    Some log in validation changes

--- MBCFM/Controllers/AccountController.cs
+++ MBCFM/Controllers/AccountController.cs
@@ -37,6 +37,7 @@ namespace MBCFM.Controllers
             }
             else
             {
+                ViewBag.Error = "Username or password is invalid.";
                 return View();
             }
         }
---
+++ MBCFM/Models/JobsInitialiser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MBCFM.Models
+{
+    public class JobsInitialiser : DropCreateDatabaseIfModelChanges<JobsContext>
+    {
+        protected override void Seed(JobsContext context)
+        {
+            context.Users.Add(new User
+                                {
+                                    UserName = "Admin",
+                                    Password = "password",
+                                    FullName = "Site Administrator"
+                                });
+            base.Seed(context);
+        }
+    }
+}
