cc5868002ef652353917e5d1690793263eefed8f
Steve Clements <steve@originaleye.co.uk>
2014-12-17 10:50:38 +0000

    changes prior to the merging of the tables work

--- MBCFM/Sql/ExtraSqlObjectsSchema.sql
+++ MBCFM/Sql/ExtraSqlObjectsSchema.sql
@@ -40,3 +40,78 @@ FROM         dbo.FMIncidentsFM261 AS J
 
 GO
 
+-- Add siteNotes to Reactive Table
+BEGIN TRANSACTION
+GO
+ALTER TABLE dbo.reactiveCallOut ADD
+	siteNotes ntext NULL
+GO
+ALTER TABLE dbo.reactiveCallOut SET (LOCK_ESCALATION = TABLE)
+GO
+COMMIT
+
+
+-- create WebJobsJoined
+
+USE [FMHelpdeskSQLV4]
+GO
+
+/****** Object:  View [dbo].[WebJobsJoined]    Script Date: 17/12/2014 10:40:30 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[WebJobsJoined]
+AS
+SELECT        
+J.[MBC Job No], 
+j.[Client Job No], 
+j.priority, 
+j.siteNo as ClientName, 
+j.currentStatus, 
+j.ordertype, 
+j.SiteTelNo as SitePhoneNo,
+j.problem, 
+j.subContractor as UserName, 
+j.note as Notes, 
+cast(j.timeOnSite as nvarchar(50)) as ArrivalTime, 
+cast(j.timeOffSite as nvarchar(50)) as DepartureTime, 
+j.materials as CostOfMaterials,
+j.Enteredby,
+j.siteName,
+j.siteAddress,
+j.siteTown,
+j.siteCounty,
+j.siteNotes
+FROM            dbo.FMIncidentsFM261 as j
+
+union all
+
+SELECT
+J.[MBC Job No], 
+j.[Client Job No], 
+j.priority, 
+j.sitename as ClientName,
+j.currentStatus, 
+j.ordertype, 
+j.Sitephone as SitePhoneNo,
+j.problem, 
+j.subContractor as UserName, 
+j.note as Notes, 
+j.timeOnSite as ArrivalTime, 
+j.timeOffSite as DepartureTime, 
+j.materials as CostOfMaterials,
+j.Enteredby,
+j.siteName,
+j.siteAddress,
+j.siteTown,
+j.siteCounty,
+j.siteNotes
+
+FROM             dbo.reactiveCallOut as j
+                        
+
+GO
+
